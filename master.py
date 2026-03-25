374158b9c4d120f15d17e56f79baa4366e535b5b
xanderrg <xrayxander@hotmail.com>
2026-03-25 10:37:05 -0600

    Update master.py
    move class

--- master.py
+++ master.py
@@ -40,6 +40,29 @@ squirtle = Pokemon("Squirtle", "Water", 65)
 # Xander pt 2
 
 
+class Move():
+    def __init__(self, move_name, elemental_type, low_attack_points, high_attack_points):
+        self.move_name = move_name
+        self.elemental_type = elemental_type
+        self.low_attack_points = low_attack_points
+        self.high_attack_points = high_attack_points
+
+    def get_info(self):
+        return f"{self.move_name} (Type: {self.elemental_type}: {self.low_attack_points} to {self.high_attack_points} Attack Points)"
+    
+    def generate_attack_value(self):
+        random_attack_points = rd.randrange(self.low_attack_points, self.high_attack_points + 1)
+        return random_attack_points
+    
+oTackle = Move("Tackle", "Normal", 5, 20)
+oQuick_attack = Move("Quick Attack", "Normal", 6, 25)
+oSlash = Move("Slash", "Normal", 10, 30)
+oFlamethrower = Move("Flamethrower", "Fire", 5, 30)
+oEmber = Move("Ember", "Fire", 10, 20)
+oWater_gun = Move("Water Gun", "Water", 5, 15)
+oHydro_pump = Move("Hydro Pump", "Water", 20, 25)
+oVine_whip = Move("Vine Whip", "Grass", 10, 25)
+oSolar_Beam = Move("Solar Beam", "Grass", 18, 27)
 
 
 
